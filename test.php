7521c3719939ce15ea26eaa87fbcf75a31a00ade
Anna Sabo <asabo002@seattlecentral.edu>
2015-11-16 16:51:01 -0800

    Initial test page made

---
+++ test.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+</head>
+
+<body>
+<h1>This is supposed to be a web page</h1>
+<p>Version 2</p>
+<p>Content would go here</p>
+<p>Content would go here</p>
+<p>Content would go here</p>
+
+</body>
+
+</html>
